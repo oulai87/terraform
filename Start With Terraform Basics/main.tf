7e7169dddf024da70af358ac7fc99ea67b26d7bc
Thomas <oulaithomas05@gmail.com>
2023-03-05 07:18:50 -0600

    updated provider

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -3,13 +3,6 @@
 # Configure the AWS Provider
 provider "aws" {
     version = "3.53.0"
-    access_key = "AKIAXC3FBB2OJJC3DUIP"
-    secret_key = "SECRET_KEY"
-    region     = "us-east-2"
+    region     = "us-east-1"
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    version = "2.72.0"
-    features {}
-}
